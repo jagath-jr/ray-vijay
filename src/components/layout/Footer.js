3564038d9bbf1ceb827e7b21167a465da3f2a9aa
Jagath J R <jagath.jr@hotmail.com>
2026-05-31 20:58:12 +0530

    Update Footer.js

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -149,7 +149,7 @@ export default function Footer() {
       href="https://www.duvitra.com/" 
       target="_blank" 
       rel="noopener noreferrer"
-      className="hover:text-[#cba328] transition-colors"
+      className="hover:text-[#cba328] underline transition-colors"
     >
       Duvitra
     </a>.
